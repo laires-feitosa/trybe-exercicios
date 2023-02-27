9b56ce0e1f5caf49de1ffb1fe0d07bf19130556f
Laíres Feitosa <lairessfeitosa@gmail.com>
2023-02-27 15:36:35 -0400

    enviando arquivos

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-1/scripts.js
@@ -0,0 +1,7 @@
+let myName = Laíres;
+let birthCity = Manaus;
+let birthYear = 1997;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-2/scripts.js
@@ -0,0 +1,11 @@
+let base = 5;
+let heigth = 8;
+let area = base * heigth;
+let perimeter = base + heigth;
+
+
+console.log(base);
+console.log(heigth);
+console.log(area);
+console.log(perimeter);
+
